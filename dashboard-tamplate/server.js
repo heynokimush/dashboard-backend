d9e8cd41a9c8d0f1c9f0b56569c59583b0918097
heymush00 <heymush00@mindwareworks.com>
2025-05-15 16:17:26 +0900

    Fix | delete dashboard router

--- dashboard-tamplate/server.js
+++ dashboard-tamplate/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dashboardRouter from "./routers/index.js";
+// import dashboardRouter from "./routers/index.js";
 import settingRouter from "./routers/settingRouter.js";
 import statisticsRouter from "./routers/statisticsRouter.js";
 import dotenv from "dotenv";
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", dashboardRouter);
+// app.use("/api", dashboardRouter);
 app.use("/api/setting", settingRouter);
 app.use("/api/statistics", statisticsRouter);
 
